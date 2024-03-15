a440e4fb9da0030f073f78994575889ae00ea5da
RafaelAlvesTech <rafaelpereiraalves@ufba.br>
2024-03-15 19:26:35 -0300

    Commitar alterações

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -12,7 +12,7 @@ Possible Results:
 Steps:
 1. Start new game
 2. Show the board and instructions. X or o save option to player 1 and player 2. 
-3 . Boar 3x3 vertical array A, B, C and horizontal 1, 2, 3.
+3 . Board 3x3 vertical array A, B, C and horizontal 1, 2, 3.
 while !winner or !draw:
 4. Get player 2 input id vertical and horizontal. Check valide ocuppied position and valide input.
 5. Get player 2 input id vertical and horizontal. Check valide ocuppied position and valide input.
@@ -22,12 +22,13 @@ while !winner or !draw:
 7. Funciton for board print
 
 
-
 ------------------------------------------------
 
 8*. Adiconal features: Game mode, player vs player, player vs computer, computer vs computer.
 9*. Adiconal features: Score board, player 1 vs player 2.
 10*. Adiconal features: Make logical with object oriented programming.
 11.* Make Minimum Big  O notation for the algorithm.
+12.* Make a GUI for the game.
 
 """
+
